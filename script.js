30df89c48b62685f0187b3e8cbd3fb5b655c7497
JumpyLionnn <jumpylionn1@gmail.com>
2022-08-11 15:15:06 +0300

    added a createElement function to avoid repeatition

--- script.js
+++ script.js
@@ -18,6 +18,28 @@ const r = () => {
 
     vodElement.currentTime = (Math.floor(timeMins * 60) + Math.floor(timeSecs));
 
+    const createElement = (tag, properties) => {
+        const element = document.createElement(tag);
+        if(properties.classList){
+            if(typeof properties.classList === "string"){
+                for(const className of properties.classList.split(" "))
+                    element.classList.add(className);
+            }
+            else if (Array.isArray(properties.classList)){ 
+                for(const className of properties.classList)
+                    element.classList.add(className);
+            }
+        }
+        if(properties.style){
+            for(const stylePropertyName in properties.style)
+                element.style.setProperty(stylePropertyName, properties.style[stylePropertyName]);
+        }
+        if(properties.text){
+            element.innerText = properties.text;
+        }
+        return element;
+    };
+
     const changeUrlForTimeStamp = () => {
         //building the parameters for the timestamp
         //then setting state so you can send the url after seekedorupdated
@@ -43,32 +65,28 @@ const r = () => {
         const urlWithoutTime  = document.URL.slice(0 ,document.URL.indexOf('&min'));
         navigator.clipboard.writeText(urlWithoutTime);
         
-    }
+    };
     const copyUrlWithTime = () => {
         navigator.clipboard.writeText(document.URL);
-    }
+    };
 
     const shareButton = document.querySelector('.shareDropDown');
-    const shareButtonNoTime = document.createElement('div');
-    const shareButtonTime = document.createElement('div');
-        shareButtonNoTime.classList.add('dropDownOptions');
-        shareButtonNoTime.innerText = ('No timestamp');
-        shareButtonTime.classList.add('dropDownOptions');
-        shareButtonTime.innerText = ('Current timestamp');
+    const shareButtonNoTime = createElement('div', {text: 'No timestamp', classList: ["dropDownOptions"]});
+    const shareButtonTime = createElement('div', {text: 'Current timestamp', classList: ["dropDownOptions"]});
         shareButtonNoTime.addEventListener('click', copyUrl);
         shareButtonTime.addEventListener('click', copyUrlWithTime);
         shareButton.appendChild(shareButtonNoTime);
         shareButton.appendChild(shareButtonTime);
 
 
-    const renderChat = () => {
-        const getTimeString = (time) => {
-            const hours = Math.floor(time / 60 / 60);
-            const minutes = Math.floor(time / 60);
-            const seconds = Math.floor(time);
-            return`${hours}:${`${(minutes - hours * 60)}`.padStart(2, 0)}:${`${(seconds - minutes * 60)}`.padStart(2, 0)}`;
-        };
+    const getTimeString = (time) => {
+        const hours = Math.floor(time / 60 / 60);
+        const minutes = Math.floor(time / 60);
+        const seconds = Math.floor(time);
+        return`${hours}:${`${(minutes - hours * 60)}`.padStart(2, 0)}:${`${(seconds - minutes * 60)}`.padStart(2, 0)}`;
+    };
 
+    const renderChat = () => {
         chatElement.innerHTML = "";
         const renderableComments = [];
         for (const comment of comments) {
@@ -81,24 +99,20 @@ const r = () => {
         for (const comment of renderableComments) {
             const commentElement = document.createElement("p");
 
-            const timeElement = document.createElement("span");
-            timeElement.innerText = getTimeString(comment.content_offset_seconds);
-            timeElement.classList.add("time");
+            const timeElement = createElement("span", {text: getTimeString(comment.content_offset_seconds), classList: ["time"]});
 
-            const displayNameElement = document.createElement("span");
-            displayNameElement.innerText = comment.commenter.display_name;
-            displayNameElement.style.setProperty("color", comment.message.user_color);
-            displayNameElement.classList.add("display-name");
+            const displayNameElement = createElement("span", {
+                text: comment.commenter.display_name, 
+                classList: ["display-name"], 
+                style: {color: comment.message.user_color}
+            });
 
-            const dividerElement = document.createElement("span");
-            dividerElement.innerText = comment.message.is_action ? "" : ":";
-            dividerElement.classList.add("divider");
+            const dividerElement = createElement("span", {text: comment.message.is_action ? "" : ":", classList: ["divider"]});
 
             const fragmentsToAppend = [];
 
             for (fragment of comment.message.fragments) {
-                const fragmentElement = document.createElement("span");
-                fragmentElement.classList.add("fragment");
+                const fragmentElement = createElement("span", {classList: ["fragment"]});
                 if ("emoticon" in fragment) {
                     //build and append the Emotes
                     const emoteElement = document.createElement("img");
